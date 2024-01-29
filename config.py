8600dfd7149762a6f49fda90d0f456fc9f0da505
dkoz <drunkenkoz@gmail.com>
2024-01-29 14:47:31 -0500

    Main Config

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+bot_token = os.getenv('BOT_TOKEN', "No token found")
+bot_prefix = os.getenv('BOT_PREFIX', "!")
+bot_activity = os.getenv('BOT_ACTIVITY', "Palworld")
---
+++ main.py
@@ -0,0 +1,23 @@
+import nextcord
+from nextcord.ext import commands
+import config
+import os
+
+intents = nextcord.Intents.all()
+bot = commands.Bot(command_prefix=config.bot_prefix, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is ready!')
+    activity = nextcord.Activity(type=nextcord.ActivityType.playing, name=config.bot_activity)
+    await bot.change_presence(activity=activity)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+if __name__ == '__main__':
+    for filename in os.listdir("cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+    bot.run(config.bot_token)
