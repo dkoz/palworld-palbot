54661f94400017c25d99681784905a665ad8b672
Kozejin <drunkenkoz@gmail.com>
2024-02-11 11:39:11 -0500

    Update restart.py

--- cogs/restart.py
+++ cogs/restart.py
@@ -43,13 +43,20 @@ class RestartCog(commands.Cog):
                     await self.broadcast_warning("Server_restart_in_5_minutes")
                 elif 180 <= time_until_shutdown < 240:  # 3 minutes before
                     await self.broadcast_warning("Server_restart_in_3_minutes")
-                elif 60 <= time_until_shutdown < 120:  # 2 minutes before with shutdown command
-                    await self.initiate_shutdown("Shutdown 120 Server_restart_in_2_minutes")
+                elif 120 <= time_until_shutdown < 180:
+                    await self.save_server_state() # Save the server state
+                elif 60 <= time_until_shutdown < 120:
+                    await self.initiate_shutdown("Shutdown 60 Server_restart_in_1_minute")
 
     async def broadcast_warning(self, message):
         for server_name, server_info in self.servers.items():
             await self.rcon_command(server_info, f"Broadcast {message}")
             print(f"Broadcasted to {server_name}: {message}")
+
+    async def save_server_state(self):
+        for server_name, server_info in self.servers.items():
+            response = await self.rcon_command(server_info, "Save")
+            print(f"State saved for {server_name}: {response}")
 
     async def initiate_shutdown(self, command):
         for server_name, server_info in self.servers.items():
