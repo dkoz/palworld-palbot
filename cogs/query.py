08b49f98f9204a9d9d583464d32c1c9815b3bcda
Kozejin <drunkenkoz@gmail.com>
2024-02-11 10:37:15 -0500

    Query Update
    Switched from nextcord task to asyncio sleep.

--- cogs/query.py
+++ cogs/query.py
@@ -1,9 +1,10 @@
 import json
 import os
 import nextcord
-from nextcord.ext import commands, tasks
+from nextcord.ext import commands
 from util.gamercon_async import GameRCON
 import util.constants as constants
+import asyncio
 
 class QueryCog(commands.Cog):
     def __init__(self, bot):
@@ -11,11 +12,7 @@ class QueryCog(commands.Cog):
         self.message_ids = {}
         self.load_config()
         self.load_message_ids()
-        self.server_status_checks = []
-        for server_name, server_config in self.servers.items():
-            self.server_status_checks.append(self.create_task(server_name, server_config))
-        for task in self.server_status_checks:
-            task.start()
+        self.create_task()
 
     def load_config(self):
         config_path = os.path.join('data', 'config.json')
@@ -33,20 +30,28 @@ class QueryCog(commands.Cog):
         with open(os.path.join('data', 'server_status.json'), 'w') as file:
             json.dump(self.message_ids, file, indent=4)
 
-    def create_task(self, server_name, server_config):
-        @tasks.loop(minutes=2)
-        async def server_status_check():
-            if "QUERY_CHANNEL" in server_config:
-                channel_id = server_config["QUERY_CHANNEL"]
+    def create_task(self):
+        for server_name, server_config in self.servers.items():
+            self.bot.loop.create_task(self.server_status_check(server_name, server_config))
+
+    async def server_status_check(self, server_name, server_config):
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed():
+            try:
+                channel_id = server_config.get("QUERY_CHANNEL")
                 channel = self.bot.get_channel(channel_id)
                 if channel:
                     status = await self.check_server_status(server_config)
                     player_count = await self.get_player_count(server_config) if status == "Online" else 0
+                    players = await self.get_player_names(server_config) if status == "Online" else []
 
-                    embed = nextcord.Embed(title=f"{server_name} Status", description=f"**Status:** {status}", color=nextcord.Color.green())
+                    embed = nextcord.Embed(title=f"{server_name} Status", description=f"**Status:** {status}", color=nextcord.Color.green() if status == "Online" else nextcord.Color.red())
                     embed.add_field(name="Players", value=f"{player_count}/32", inline=False)
                     embed.add_field(name="Connection Info", value=f"```{server_config['RCON_HOST']}:{server_config['SERVER_PORT']}```", inline=False)
                     embed.set_footer(text=constants.FOOTER_TEXT, icon_url=constants.FOOTER_IMAGE)
+
+                    player_list = '\n'.join(players) if players else "No players online"
+                    players_embed = nextcord.Embed(title=f"Players Online", description=player_list, color=nextcord.Color.blue())
 
                     message_key = f"{server_name}_{channel_id}"
                     message_id = self.message_ids.get(message_key)
@@ -61,38 +66,23 @@ class QueryCog(commands.Cog):
                         message = await channel.send(embed=embed)
                         self.message_ids[message_key] = message.id
 
-                if status == "Online":
-                    players = await self.get_player_names(server_config)
-                    if players:
-                        player_list = '\n'.join(players)
-                        description = f"{player_list}"
+                    player_message_key = f"{server_name}_{channel_id}_players"
+                    player_message_id = self.message_ids.get(player_message_key)
+                    if player_message_id:
+                        try:
+                            player_message = await channel.fetch_message(player_message_id)
+                            await player_message.edit(embed=players_embed)
+                        except nextcord.NotFound:
+                            player_message = await channel.send(embed=players_embed)
+                            self.message_ids[player_message_key] = player_message.id
                     else:
-                        description = "No players online"
-                else:
-                    description = "Server Offline"
-
-                players_embed = nextcord.Embed(title=f"Players Online", description=description, color=nextcord.Color.blue())
-
-                player_message_key = f"{server_name}_{channel_id}_players"
-                player_message_id = self.message_ids.get(player_message_key)
-                if player_message_id:
-                    try:
-                        player_message = await channel.fetch_message(player_message_id)
-                        await player_message.edit(embed=players_embed)
-                    except nextcord.NotFound:
                         player_message = await channel.send(embed=players_embed)
                         self.message_ids[player_message_key] = player_message.id
-                else:
-                    player_message = await channel.send(embed=players_embed)
-                    self.message_ids[player_message_key] = player_message.id
 
-                self.save_message_ids()
-
-        @server_status_check.before_loop
-        async def before_server_status_check():
-            await self.bot.wait_until_ready()
-
-        return server_status_check
+                    self.save_message_ids()
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            await asyncio.sleep(60)
 
     async def check_server_status(self, server_config):
         try:
