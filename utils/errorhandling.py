93518140fa9983be97e34aa202790caca5526341
Kozejin <2613841+dkoz@users.noreply.github.com>
2024-10-01 02:04:54 -0400

    Log System Improvements

--- utils/errorhandling.py
+++ utils/errorhandling.py
@@ -12,17 +12,30 @@ def setup_logging():
         os.makedirs('logs')
 
     log_filename = f"palbot_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+    log_path = os.path.join('logs', log_filename)
+
     log_handler = RotatingFileHandler(
-        filename=os.path.join('logs', log_filename),
-        maxBytes=0, 
-        backupCount=5,
+        filename=log_path, 
+        maxBytes=10**7,
+        backupCount=6,
         encoding='utf-8'
     )
-
     log_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
     log_handler.setFormatter(log_formatter)
 
-    logging.basicConfig(handlers=[log_handler], level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, handlers=[log_handler])
+
+    clean_old_logs('logs', 10)
+
+def clean_old_logs(directory, max_logs):
+    log_files = sorted(
+        [os.path.join(directory, f) for f in os.listdir(directory) if f.startswith("palbot_") and f.endswith(".log")],
+        key=os.path.getctime,
+        reverse=True
+    )
+
+    while len(log_files) > max_logs:
+        os.remove(log_files.pop())
 
 async def handle_errors(interaction, error):
     try:
