8a0c6ccd79497fb70e5fc967fbbd0e704050623c
Kozejin <2613841+dkoz@users.noreply.github.com>
2024-10-13 22:59:56 -0400

    Version Bump

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,7 @@
 FOOTER_IMAGE = "https://palbot.gg/assets/images/rexavatar.png"
 FOOTER_TEXT = "Powered by Palbot"
 TITLE_URL = "https://github.com/dkoz/palworld-palbot"
-PALBOT_VERSION = "v0.3.3"
+PALBOT_VERSION = "v0.3.4"
 PALBOT_ART = r"""
 __________        .__ ___.           __   
 \______   \_____  |  |\_ |__   _____/  |_ 
